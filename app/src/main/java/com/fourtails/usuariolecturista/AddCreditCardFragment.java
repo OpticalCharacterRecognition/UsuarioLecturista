178c3bdf6f550a1e622a2951b8d878616b834134
Carlos Labrado <labrado.carlos@gmail.com>
2014-11-26 13:34:16 -0700

    payment options and add card 2

---
+++ app/src/main/java/com/fourtails/usuariolecturista/AddCreditCardFragment.java
@@ -0,0 +1,166 @@
+package com.fourtails.usuariolecturista;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.fourtails.usuariolecturista.utilities.DatePickerFragmentCreditCard;
+
+import java.util.Calendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link AddCreditCardFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link AddCreditCardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AddCreditCardFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    TextView textViewCreditCardExpirationDate;
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AddCreditCardFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AddCreditCardFragment newInstance(String param1, String param2) {
+        AddCreditCardFragment fragment = new AddCreditCardFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public AddCreditCardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_credit_card, container, false);
+
+
+        textViewCreditCardExpirationDate = (TextView) view.findViewById(R.id.editTextCreditCardExpirationDate);
+
+        textViewCreditCardExpirationDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
+
+        return view;
+    }
+
+    /**
+     * This will call for an special date picker without the "day"
+     */
+    private void showDatePicker() {
+        DatePickerFragmentCreditCard date = new DatePickerFragmentCreditCard();
+        /**
+         * Set Up Current Date Into dialog
+         */
+        Calendar calendar = Calendar.getInstance();
+        Bundle args = new Bundle();
+        args.putInt("year", calendar.get(Calendar.YEAR));
+        args.putInt("month", calendar.get(Calendar.MONTH));
+        args.putInt("day", calendar.get(Calendar.DAY_OF_MONTH));
+        args.putLong("currentDateInMillis", calendar.getTimeInMillis());
+        date.setArguments(args);
+        /**
+         * Set Call back to capture selected date
+         */
+        date.setCallBack(ondate);
+        date.show(getFragmentManager(), "Date Picker");
+
+    }
+
+    DatePickerDialog.OnDateSetListener ondate = new DatePickerDialog.OnDateSetListener() {
+
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+
+            textViewCreditCardExpirationDate.setText(String.valueOf(monthOfYear + 1)
+                    + "-" + String.valueOf(year));
+            //Toast.makeText(getActivity(), String.valueOf(dayOfMonth) + "-" + String.valueOf(monthOfYear + 1) + "-" + String.valueOf(year), Toast.LENGTH_LONG).show();
+
+        }
+    };
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
---
+++ app/src/main/java/com/fourtails/usuariolecturista/utilities/DatePickerFragmentCreditCard.java
@@ -0,0 +1,73 @@
+package com.fourtails.usuariolecturista.utilities;
+
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DatePickerFragmentCreditCard extends DialogFragment {
+
+    DatePickerDialog.OnDateSetListener ondateSet;
+
+    public DatePickerFragmentCreditCard() {
+        // Required empty public constructor
+    }
+
+    public void setCallBack(DatePickerDialog.OnDateSetListener ondate) {
+        ondateSet = ondate;
+    }
+
+    private int year, month, day;
+    private long currentDateInMillis;
+
+    @SuppressLint("NewApi")
+    @Override
+    public void setArguments(Bundle args) {
+        super.setArguments(args);
+        year = args.getInt("year");
+        month = args.getInt("month");
+        day = args.getInt("day");
+        currentDateInMillis = args.getLong("currentDateInMillis");
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), ondateSet,
+                year,
+                month,
+                day) {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                int year = getContext().getResources()
+                        .getIdentifier("android:id/day", null, null);
+                if (year != 0) {
+                    View yearPicker = findViewById(year);
+                    if (yearPicker != null) {
+                        yearPicker.setVisibility(View.GONE);
+                    }
+                }
+                int calendarView = getContext().getResources()
+                        .getIdentifier("android:id/calendar_view", null, null);
+                if (calendarView != 0) {
+                    View calendarViewPicker = findViewById(calendarView);
+                    if (calendarViewPicker != null) {
+                        calendarViewPicker.setVisibility(View.GONE);
+                        calendarViewPicker.setEnabled(false);
+                    }
+                }
+            }
+        };
+        datePickerDialog.getDatePicker().setMinDate(currentDateInMillis);
+        datePickerDialog.setTitle("");
+        return datePickerDialog;
+    }
+
+}
