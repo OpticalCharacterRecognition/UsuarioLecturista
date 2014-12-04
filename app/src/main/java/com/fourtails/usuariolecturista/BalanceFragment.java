ac3c1f20ef14e42c092064180023875b5b3153b8
Carlos Labrado <labrado.carlos@gmail.com>
2014-12-04 16:37:18 -0700

    buttons on main page

--- app/src/main/java/com/fourtails/usuariolecturista/BalanceFragment.java
+++ app/src/main/java/com/fourtails/usuariolecturista/BalanceFragment.java
@@ -3,6 +3,7 @@ package com.fourtails.usuariolecturista;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.Uri;
@@ -11,7 +12,6 @@ import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,9 +26,11 @@ import com.facebook.model.OpenGraphAction;
 import com.facebook.model.OpenGraphObject;
 import com.facebook.widget.FacebookDialog;
 import com.facebook.widget.WebDialog;
+import com.fourtails.usuariolecturista.ocr.CaptureActivity;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.GraphViewSeries;
 import com.jjoe64.graphview.LineGraphView;
+import com.melnykov.fab.FloatingActionButton;
 
 import java.util.Date;
 import java.util.Random;
@@ -147,22 +149,30 @@ public class BalanceFragment extends Fragment {
 
         //Button resetValues = (Button) view.findViewById(R.id.buttonResetValuesForCycle);
 
-        Button payButton = (Button) view.findViewById(R.id.buttonPay);
-        payButton.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabPay = (FloatingActionButton) view.findViewById(R.id.fabPay);
+        fabPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 payButtonClicked();
             }
         });
 
-        //facebook open graph
-        Button facebookOpenGraph = (Button) view.findViewById(R.id.buttonFBOpenGraph);
-        facebookOpenGraph.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fabScan = (FloatingActionButton) view.findViewById(R.id.fabScan);
+        fabScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGraphClicked();
+                scanButtonClicked();
             }
         });
+
+        //facebook open graph
+//        Button facebookOpenGraph = (Button) view.findViewById(R.id.buttonFBOpenGraph);
+//        facebookOpenGraph.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openGraphClicked();
+//            }
+//        });
 
 
         //facebook share
@@ -369,6 +379,11 @@ public class BalanceFragment extends Fragment {
 //        }.execute();
 
         return view;
+    }
+
+    private void scanButtonClicked() {
+        Intent ocrCaptureActivity = new Intent(getActivity(), CaptureActivity.class);
+        startActivity(ocrCaptureActivity);
     }
 
     private void payButtonClicked() {
